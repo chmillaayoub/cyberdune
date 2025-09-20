94d52994abc9c79ae98a975a4f51b82178524707
Ayoub Chmilla <ayoub.chmilla.30@edu.uiz.ac.ma>
2025-09-20 12:10:20 +0100

    logos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,11 @@ export const metadata: Metadata = {
       { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
       { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
     ],
-    apple: "/icon-192.png", // iOS shortcut icon
+    apple: "/apple-icon.png", // iOS shortcut icon
   },
   openGraph: {
     title: "CyberDune",
-    images: ["/icon-512.png"], // large one for link previews
+    images: ["/apple-icon.png"], // large one for link previews
   },
 }
 
